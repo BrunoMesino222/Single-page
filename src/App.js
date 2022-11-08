9e49232e05c1414ff8aa4657c0b1508fb5e50a38
BrunoSimone <brunosimone.mdq@gmail.com>
2022-11-08 16:23:11 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import ReactSwitch from 'react-switch';
+import { useState } from "react";
+import { useThemeContext } from './context/ThemeContext';
 
 function App() {
+  const {contextTheme, setContextTheme} = useThemeContext()
+
+  const [checked, setChecked] = useState(false)
+  const handleSwitch = (nextChecked) => {
+    setContextTheme((state) => (state === "light"? "dark":"light"))
+    setChecked(nextChecked)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header className="App-header" id={contextTheme}>
+        <ReactSwitch 
+            onChange={handleSwitch}
+            checked={checked}
+            onColor="#86d3ff"
+            onHandleColor="#2693e6"
+            handleDiameter={30}
+            uncheckedIcon={false}
+            checkedIcon={false}
+            boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+            activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+            height={20}
+            width={48}
+            className="react-switch"
+            id="material-switch"/>
+        <h2>{contextTheme} mode</h2>
+        <img src={logo} className="App-logo" alt="logo"/>
         <a
           className="App-link"
           href="https://reactjs.org"
